56cb0fbd40c9815deca56e19dfe076d70ab47fad
unknown <luc1497@gmail.com>
2023-09-28 16:58:08 -0300

    Definição dos dias da semana

--- consultaClima.js
+++ consultaClima.js
@@ -13,8 +13,34 @@ if ("geolocation" in navigator){
                 console.log(tempo);
             })
 
+            var data = new Date();
+            var diaexten = "teste";
+            dia = data.getDay();
+            if (dia==0){
+                var diaexten = "Domingo";
+            }
+            if (dia==1){
+                var diaexten = "Segunda";
+            }
+            if (dia==2){
+                var diaexten = "Terça";
+            }
+            if (dia==3){
+                var diaexten = "Quarta";
+            }
+            if (dia==4){
+                var diaexten = "Quinta";
+            }
+            if (dia==5){
+                var diaexten = "Sexta";
+            }
+            if (dia==6){
+                var diaexten = "Sábado";
+            }
 
-
+            console.log(diaexten);
+            console.log(dia);
+            
 
 
 
